695331c4a17dcec8235e2794b4975e2ebf5029ee
cara ottmar <cara.ottmar@gmail.com>
2018-11-06 11:23:56 -0800

    landing page done.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-// import About from 
-// import Portfolio
+
 
 class Header extends React.Component {
   render() {
