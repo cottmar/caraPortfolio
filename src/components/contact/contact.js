55551d2117442c0297ff55f3fced31b67052b164
cara ottmar <cara.ottmar@gmail.com>
2018-11-07 13:04:27 -0800

    contact and home page done, just need to do portfolio page.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './contact.css';
 
 class Contact extends React.Component {
   render() {
     return (
-      <div className='contact'>
-        <p>PUT GITHUB AND LINKEDIN LINKS HERE</p>
+      <div className='contactBlurb'>
+        <h1>I'm always happy to hear from you.</h1>
+        <h2>cara.ottmar@gmail.com</h2>
+        <ul>
+          <a href="https://github.com/cottmar">Github</a>
+        </ul>
+        <ul>
+          <a href="https://www.linkedin.com/in/cara-ottmar/">Linkedin</a>
+        </ul>
       </div>
     )
   }
