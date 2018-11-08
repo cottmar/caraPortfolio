aeefe693d4be5b39158e4607fea89ae18fac8644
cara ottmar <cara.ottmar@gmail.com>
2018-11-07 22:53:01 -0800

    working on portfolio carousel

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,41 +1,41 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import finders from '../../assets/finders/Screen Shot 2018-11-05 at 11.07.56 PM.png';
-// import dogs from '../../assets/dogs/Screen Shot 2018-11-05 at 11.06.55 PM.png';
-// import quiz from '../../assets/quiz/Screen Shot 2018-11-05 at 11.09.24 PM.png';
-// import sossms from '../../assets/sossms/Screen Shot 2018-11-05 at 11.07.30 PM.png';
-// import strafford from '../../assets/strafford/0.png';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import finders from '../../assets/finders/Screen Shot 2018-11-05 at 11.07.56 PM.png';
+import dogs from '../../assets/dogs/Screen Shot 2018-11-05 at 11.06.55 PM.png';
+import quiz from '../../assets/quiz/Screen Shot 2018-11-05 at 11.09.24 PM.png';
+import sossms from '../../assets/sossms/Screen Shot 2018-11-05 at 11.07.30 PM.png';
+import strafford from '../../assets/strafford/0.png';
 
-// class Portfolio extends React.Component {
-//   render() {
-//     return (
-//       <div className="projects">
-//       {/* <Carousel/> */}
-//       <h1>MY PROJECTS</h1>
-//         <div className="finders">
-//           <h2><a href='https://finders-keepers-frontend.herokuapp.com/'></a><img src={finders} alt="game play" width='90px'/></h2>
-//           <p>Full stack real-time, multiplayer game where users compete against the clock to find the most stars. Utilizing socket.io, ReactJS</p>
-//         </div>
-//         <div className="strafford">
-//           <h2><a href='https://strafford-acres.herokuapp.com/'></a><img src={strafford} alt="website screenshot" width='90px'/></h2>
-//           <p>Client application advertising acreage marketed as "For Sale by Owner.</p>
-//         </div>
-//         <div className="PlentyofDogs">
-//           <h2><a href='http://plenty-of-dogs.site/'></a><img src={dogs} alt="dog site" width='90px'/></h2>
-//           <p>Application designed to help you select a breed of dog, matching you with available adoptable rescue dogs in the Seattle, WA area.</p>
-//         </div>
-//         <div className="SosSms">
-//           <h2><a href='https://www.npmjs.com/package/sos-sms'></a><img src={sossms} alt="npm screenshot" width='90px'/></h2>
-//           <p>Easily enable an application to instantly notify developers of critical errors via SMS messages. Included custom NPM package.</p>
-//         </div>
-//         <div className="Quiz">
-//           <h2><a href='https://ggrieb.github.io/death-becomes-you/learn.html#sea'></a><img src={quiz} alt="quiz" width='90px'/></h2>
-//           <p>Tongue-in-cheek quiz designed to help you decide what burial option to choose when "Death Becomes You" - a technological solution to a real-world problem.</p> */}
-//          <Link to='/about'><button>learn about me</button></Link>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <div className="projects">
+      {/* <Carousel/> */}
+      <h1>MY PROJECTS</h1>
+        <div className="finders">
+          <h2><a href='https://finders-keepers-frontend.herokuapp.com/'></a><img src={finders} alt="game play" width='90px'/></h2>
+          <p>Full stack real-time, multiplayer game where users compete against the clock to find the most stars. Utilizing socket.io, ReactJS</p>
+        </div>
+        <div className="strafford">
+          <h2><a href='https://strafford-acres.herokuapp.com/'></a><img src={strafford} alt="website screenshot" width='90px'/></h2>
+          <p>Client application advertising acreage marketed as "For Sale by Owner.</p>
+        </div>
+        <div className="PlentyofDogs">
+          <h2><a href='http://plenty-of-dogs.site/'></a><img src={dogs} alt="dog site" width='90px'/></h2>
+          <p>Application designed to help you select a breed of dog, matching you with available adoptable rescue dogs in the Seattle, WA area.</p>
+        </div>
+        <div className="SosSms">
+          <h2><a href='https://www.npmjs.com/package/sos-sms'></a><img src={sossms} alt="npm screenshot" width='90px'/></h2>
+          <p>Easily enable an application to instantly notify developers of critical errors via SMS messages. Included custom NPM package.</p>
+        </div>
+        <div className="Quiz">
+          <h2><a href='https://ggrieb.github.io/death-becomes-you/learn.html#sea'></a><img src={quiz} alt="quiz" width='90px'/></h2>
+          <p>Tongue-in-cheek quiz designed to help you decide what burial option to choose when "Death Becomes You" - a technological solution to a real-world problem.</p> */}
+         {/* <Link to='/about'><button>learn about me</button></Link> */}
+        </div>
+      </div>
+    )
+  }
+}
 
-// export default Portfolio
+export default Portfolio
