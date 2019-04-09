4535d92d85e0bd9b2dffbfd27c07446d6de2384a
cara ottmar <cara.ottmar@gmail.com>
2019-04-08 23:21:26 -0700

    hyperlinked images, added pdf resume instead of markdown.

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import me from '../../assets/me/00100sPORTRAIT_00100_BURST20180917083737026_COVER.jpg';
+import resume from '../../assets/resume/Cara Ottmar - Resume pdf.pdf';
 import './landing.css';
 
 class Landing extends React.Component {
@@ -13,7 +14,7 @@ class Landing extends React.Component {
           <h1>Nice to meet you</h1>
             <h2>My name is <span>Cara Ottmar</span>. I'm a <span>Web Developer</span> with Fullstack skills (MERN). I'm passionate about working with mission based organizations, building software and applications that help solve problems and improve lives.<br></br>
             </h2>
-            <a href="https://github.com/cottmar/markdownResume/blob/master/resume.md" target="_blank" rel="noopener noreferrer">Resume</a>
+            <a href={resume} target="_blank">Resume</a>
       </div>
     </div>
     )
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -13,30 +13,30 @@ class Portfolio extends React.Component {
           <div className="row">
             <div className="column">
               <div className="content">
-                <a href="https://github.com/spyosaurus/finders-keepers-frontend" target="_blank" ><img src={finders} alt="game"></img></a>
-                <h3>Finders Keepers</h3>
-                <p>Full stack real-time, multiplayer game where users compete against the clock to find the most stars. Utilizing socket.io, ReactJS</p>
+                <a href="https://finders-keepers-frontend.herokuapp.com/" target="_blank"><img src={finders} alt="game"></img></a>
+                <a href="https://github.com/spyosaurus/finders-keepers-frontend" target="_blank" ><h3>Finders Keepers</h3></a>
+                <p>Full stack real-time, multiplayer game where users compete against the clock to find the most stars. Utilizing socket.io, ReactJS. Team project.</p>
               </div>
             </div>
             <div className="column">
               <div className="content">
-                <a href="https://github.com/plenty-of-dogs" target="_blank" rel="noopener noreferrer"><img src={dogs} alt="dogs"></img></a>
-                <h3>Plenty of Dogs</h3>
-                <p>Application designed to help you select a breed of dog, matching you with available adoptable rescue dogs in the Seattle, WA area.</p>
+              <a href="https://github.com/plenty-of-dogs" target="_blank" rel="noopener noreferrer"><img src={dogs} alt="dogs"></img></a>
+               <a href="https://github.com/plenty-of-dogs" target="_blank" rel="noopener noreferrer"><h3>Plenty of Dogs</h3></a>
+                <p>Application designed to help you select a breed of dog, matching you with available adoptable rescue dogs in the Seattle, WA area. Team project.</p>
               </div>
             </div>
             <div className="column">
               <div className="content">
-                <a href="https://github.com/401-Team-Awesome" target="_blank" rel="noopener noreferrer"><img src={sossms} alt="npmjs"></img></a>
-                <h3>SOS-SMS (npm)</h3>
-                <p>Easily enable an application to instantly notify developers of critical errors via SMS messages. Included custom NPM package.</p>
+                <a href="https://www.npmjs.com/package/sos-sms" target="_blank"><img src={sossms} alt="npmjs"></img></a>
+                <a href="https://github.com/401-Team-Awesome" target="_blank" rel="noopener noreferrer"><h3>SOS-SMS (npm)</h3></a>
+                <p>Easily enable an application to instantly notify developers of critical errors via SMS messages. Included custom NPM package. Team project.</p>
               </div>
             </div>
             <div className="column">
               <div className="content">
-                <a href="https://github.com/cottmar/straffordLandWebsite" target="_blank" rel="noopener noreferrer"><img src={strafford} alt="land"></img></a>
-                <h3>Land Ad</h3>
-                <p id="lastP">Client application advertising acreage marketed as "For Sale by Owner</p>
+                <a href="https://strafford-acres.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={strafford} alt="land"></img></a>
+                <a href="https://github.com/cottmar/straffordLandWebsite" target="_blank" rel="noopener noreferrer"><h3>Land Ad</h3></a>
+                <p id="lastP">Client application advertising acreage marketed as "For Sale by Owner." Working with a client to collect all information and assets. Wireframing, design, and build using ReactJS.</p>
               </div>
             </div>
           </div>
