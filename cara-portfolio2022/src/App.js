641a98eafd1c93c1998ee432e69f002df5aa6808
cottmar <cara.ottmar@gmail.com>
2022-05-02 22:17:24 -0700

    beginning landing page, sussing out design ideas.

---
+++ cara-portfolio2022/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import glacier from './assets/headerPic/glacier.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h1>HE<br></br>LLO<span>.</span></h1>
+        {/* <img className='header-image-glacier' src={glacier} alt="mountain in background with glacial lake in foreground"/> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
