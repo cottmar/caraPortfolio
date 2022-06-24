4289d22c3eb18100cfbef3bbb0ba03881886fe84
cottmar <cara.ottmar@gmail.com>
2022-06-23 18:54:44 -0700

    added scrollY event listener.

---
+++ cara-portfolio2022/src/AboutMe/AboutMe.js
@@ -0,0 +1,17 @@
+import glacier from '../assets/headerPic/glacier.jpg';
+import './AboutMe.css'
+
+const AboutMe = () => {
+    return (
+    // <div className='banner-image'>
+    //     <img alt="Photo of mountain in Glacier National Park" src={glacier} />
+    // </div>
+    <div>
+        <div>
+        <h1>My name is Cara</h1>
+        </div>
+     </div>
+    )
+}
+
+export default AboutMe;
--- cara-portfolio2022/src/App.js
+++ cara-portfolio2022/src/App.js
@@ -1,13 +1,34 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header/Header';
+import AboutMe from './AboutMe/AboutMe';
 
 function App() {
+
+  useEffect(() => {
+    const handleScroll = event => {
+      console.log('window.scrollY', window.scrollY);
+
+      if (window.scrollY > 0) {
+        // change CSS property
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      {/* <div className='white-outline'>
-            <p className='name'>Cara Ottmar</p>
-      </div> */}
+    <div className="App" style={{ border: '3px solid black', width: '400px', height: '100rem'}}>
       <Header />
+      <AboutMe />
+      <p>Scrolling</p>
+      <p>Scrolling</p>
+      <p>Scrolling</p>
+      <p>Scrolling</p>
+      <p>Scrolling</p>
     </div>
   );
 }
