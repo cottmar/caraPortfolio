6e3b4c931ac4a59b0a849cf28a37972457176852
cottmar <cara.ottmar@gmail.com>
2022-06-23 19:14:58 -0700

    saving

--- cara-portfolio2022/src/App.js
+++ cara-portfolio2022/src/App.js
@@ -1,36 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { ReactDOM } from 'react-dom';
+// need to import styles???
 import './App.css';
 import Header from './Header/Header';
 import AboutMe from './AboutMe/AboutMe';
 
-function App() {
+const App = () => {
 
-  useEffect(() => {
-    const handleScroll = event => {
-      console.log('window.scrollY', window.scrollY);
+    const [show, setShow] = useState({
+      itemTwo: false,
+    })
+    const [percentShown, setPercentShown] = useState({
+      itemTwo: 0,
+    })
+    const refTwo = useRef(null)
 
-      if (window.scrollY > 0) {
-        // change CSS property
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
+    useLayoutEffect(() => {
+      const topPos = (element) => console.log(element, 'what is element')
+    })
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
-    <div className="App" style={{ border: '3px solid black', width: '400px', height: '100rem'}}>
+    <div className="App">
       <Header />
       <AboutMe />
-      <p>Scrolling</p>
-      <p>Scrolling</p>
-      <p>Scrolling</p>
-      <p>Scrolling</p>
-      <p>Scrolling</p>
     </div>
   );
 }
 
 export default App;
+
+
+
+// import React, { useEffect } from 'react';
+// import './App.css';
+// import Header from './Header/Header';
+// import AboutMe from './AboutMe/AboutMe';
+
+// function App() {
+
+//   useEffect(() => {
+//     const handleScroll = event => {
+//       console.log('window.scrollY', window.scrollY);
+
+//       if (window.scrollY > 0) {
+//         // change CSS property
+//       }
+//     };
+//     window.addEventListener('scroll', handleScroll);
+
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <div className="App" style={{ border: '3px solid black', width: '400px', height: '100rem'}}>
+//       <Header />
+//       <AboutMe />
+//       <p>Scrolling</p>
+//       <p>Scrolling</p>
+//       <p>Scrolling</p>
+//       <p>Scrolling</p>
+//       <p>Scrolling</p>
+//     </div>
+//   );
+// }
+
+// export default App;
