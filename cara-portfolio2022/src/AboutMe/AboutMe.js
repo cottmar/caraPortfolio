233b8918d1ce6b0b96a35f8208338f04d9d607cd
cottmar <cara.ottmar@gmail.com>
2022-08-29 16:55:50 -0700

    saving

--- cara-portfolio2022/src/AboutMe/AboutMe.js
+++ cara-portfolio2022/src/AboutMe/AboutMe.js
@@ -3,12 +3,10 @@ import './AboutMe.css'
 
 const AboutMe = () => {
     return (
-    // <div className='banner-image'>
-    //     <img alt="Photo of mountain in Glacier National Park" src={glacier} />
-    // </div>
-    <div>
-        <div>
-        <h1>My name is Cara</h1>
+    <div className='container'>
+        <div className='aboutMe'>
+            <br></br>
+            <p>I made a huge leap in 2018, attending a coding bootcamp in Seattle, WA. My journey has taken me from a Teaching Assistant, Intern all the way to a Full Stack Web Developer. Since making the career change, I've been building a passion for Web Accessibility, and I hope to work with mission based organizations on collaborating and creating accessible and innovative products that help improve lives and the world we live in. Please take a look at some of my professional and personal projects.</p>
         </div>
      </div>
     )
--- cara-portfolio2022/src/Header/Header.js
+++ cara-portfolio2022/src/Header/Header.js
@@ -3,10 +3,15 @@ import './Header.css';
 
 const Header = () => {
     return (
-    <div className='App-header'>
-      <header className='App-greeting'>
-        <h1 className="hello-greeting">HE<br></br>LLO<span className="period-accent">.</span></h1>
-      </header>
+    <div className='container-fluid'>
+      <div>
+        <header className='App-greeting'>
+          <h1 className="hello-greeting">HE<br></br>LLO<span className="period-accent">.</span></h1>
+            <div className='introduction'>
+              <p>My name is Cara. I'm a React Developer.</p>
+            </div>
+        </header>
+      </div>
     </div>
     )
 }
